2f455dbe3917caecdd78993edc07ec3a2afae851
Raheel94 <muha.raheel94@gmail.com>
2023-12-03 11:26:58 +0500

    go

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+// Exercise 1:  Write a program that calculates the area of a rectangle.
+function area(num1, num2) {
+    console.log("The area of the rectangle is", num1 * num2);
+}
+area(12, 24);
